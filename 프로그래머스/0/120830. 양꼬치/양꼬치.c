407db4944728984c81cead5327b15c1215bfc97a
yudinee <144875860+yudinee@users.noreply.github.com>
2026-02-27 17:31:59 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int answer = n * 12000 + k * 2000;
+    
+        answer = answer - n/10 * 2000;
+    
+    
+    return answer;
+}
