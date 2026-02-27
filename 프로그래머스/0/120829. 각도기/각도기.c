859551a496773ed99a2989c04973842da4ed88ca
yudinee <144875860+yudinee@users.noreply.github.com>
2026-02-27 16:55:12 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int angle) {
+    int answer = 0;
+    
+    if(angle>0 && angle<90){
+        return answer = 1;
+    }else if (angle == 90){
+        return answer = 2;
+    }else if(angle>90 && angle < 180){
+        return answer = 3;
+    }else if(angle == 180){
+        return answer = 4;
+    }
+    
+    return answer;
+}
